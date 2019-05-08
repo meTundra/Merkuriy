99915d8fd5ad096617df5ad70fc4c16fd1c8f919
Lednev <iLednevii@gmail.com>
2019-05-08 11:24:27 +0300

    Сделан баннер и интерактив, поп апы

--- source/js/script.js
+++ source/js/script.js
@@ -1,13 +1,8 @@
 'use strict';
 
+//Окно поиска
 var searchButton = document.querySelector('.main-nav__search-button');
 var searchInput = document.querySelector('.main-nav__search-container');
-var openNav = document.querySelector('.main-nav__open-nav');
-var categories = document.querySelector('.main-nav__categories');
-var categoriesItems = document.querySelectorAll('.nav-categories__link');
-var categoriesNestedLists = document.querySelectorAll('.nav-categories__nested-list');
-var categoriesNestedItems = document.querySelectorAll('.nav-categories__nested-item');
-var categoriesDeepLists = document.querySelectorAll('.nav-categories__deep-list');
 
 searchButton.addEventListener('click', function (evt) {
   if (window.innerWidth < 760) {
@@ -16,6 +11,14 @@ searchButton.addEventListener('click', function (evt) {
     searchInput.firstChild.placeholder = 'Поиск';
   }
 });
+
+//Окно нафигации по категориям
+var openNav = document.querySelector('.main-nav__open-nav');
+var categories = document.querySelector('.main-nav__categories');
+var categoriesItems = document.querySelectorAll('.nav-categories__link');
+var categoriesNestedLists = document.querySelectorAll('.nav-categories__nested-list');
+var categoriesNestedItems = document.querySelectorAll('.nav-categories__nested-item');
+var categoriesDeepLists = document.querySelectorAll('.nav-categories__deep-list');
 
 openNav.addEventListener('click', function (evt) {
   if (window.innerWidth < 760) {
@@ -53,4 +56,77 @@ categoriesNestedItems.forEach(function (item, index) {
   })
 });
 
-console.log();
+//Поп апы
+function closePopup () {
+  popups.forEach(function (item) {
+    if (item.classList.contains('js-show')) {
+      item.classList.remove('js-show');
+    }
+  })
+}
+
+var popups = document.querySelectorAll('.popups__popup-overlay');
+var closeButtons = document.querySelectorAll('.closeButton');
+var overlays = document.querySelectorAll('.popups__popup-overlay');
+var cityButton = document.querySelector('.main-info__different-city');
+var cityPopup = document.querySelector('.city__overlay');
+
+var callbackButton = document.querySelector('.main-info__order-callback');
+var callbackPopup = document.querySelector('.callback__overlay');
+
+var authButton = document.querySelectorAll('.sign-in');
+var authPopup = document.querySelector('.auth__overlay');
+
+var quickViewButton = document.querySelectorAll('.popular-product__quick-view');
+var quickViewOverlay = document.querySelector('.quick-view__overlay');
+
+closeButtons.forEach((function (item) {
+  item.addEventListener('click', closePopup)
+}));
+
+overlays.forEach(function (item) {
+  item.addEventListener('click', function (evt) {
+    if (evt.target.classList.contains('popups__popup-overlay')) {
+      closePopup();
+    }
+  })
+});
+
+document.addEventListener('keydown', function (evt) {
+  if (evt.code === 'Escape') {
+    closePopup();
+  }
+});
+
+cityButton.addEventListener('click', function (evt) {
+  evt.preventDefault();
+  cityPopup.classList.add('js-show');
+});
+
+callbackButton.addEventListener('click', function (evt) {
+  evt.preventDefault();
+  callbackPopup.classList.add('js-show');
+});
+
+authButton.forEach(function (item) {
+  item.addEventListener('click', function (evt) {
+    evt.preventDefault();
+    authPopup.classList.add('js-show');
+  });
+});
+
+quickViewButton.forEach(function (item) {
+  item.addEventListener('click', function (evt) {
+    evt.preventDefault();
+    quickViewOverlay.classList.add('js-show');
+  });
+});
+
+
+var banner = document.querySelector('.banner');
+
+window.addEventListener("scroll", function () {
+  if (window.innerWidth > 760) {
+    banner.style.backgroundPosition = '50%' + window.scrollY / -4 + 'px';
+  }
+});
